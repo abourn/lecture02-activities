13eb3cc997b3d80053c71f2ab9ed1e0023cbefad
Adam Bourn <abourn@uw.edu>
2017-03-30 15:19:32 -0700

    finish lecture

--- android/ActivityDemo/app/src/main/java/edu/uw/activitydemo/MainActivity.java
+++ android/ActivityDemo/app/src/main/java/edu/uw/activitydemo/MainActivity.java
@@ -2,12 +2,49 @@ package edu.uw.activitydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v(TAG, "clicked!");
+            }
+        });
+
+        Log.v(TAG, "onCreate was called");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume was called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -6,24 +6,47 @@ import java.util.*;
  * A demo of GUI programming with Swing
  */
 public class MyGUI extends JFrame {
-	
+
 	public MyGUI() {
 		super("The Skywalker Family");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JButton theButton = new JButton("Click me!");
 
+		theButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Click click!");
+			}
+		});
 
 
+		JButton theButton2 = new JButton("Second!");
 
-		
-		this.add(theButton);
-		
+		theButton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Click!");
+			}
+		});
+
+		JPanel container = new JPanel();
+		container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
+		container.add(theButton);
+		container.add(theButton2);
+
+		this.add(container);
 		this.pack();
-		this.setVisible(true);		
+		this.setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
     new MyGUI(); //create the frame
   }
 }
+
+/*
+A few ways to register a listener. You can either:
+	1) Have MyGui implement ActionListener
+	2) Setup a new inner class that implements ActionListener
+	3) Register ActionListener anonymously on the button itself. (Like an anonymous
+		callback function).
+*/
